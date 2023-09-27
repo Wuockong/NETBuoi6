472216f6518b56451938b1ca60491485714c0bb4
Bui Quoc Cong <congbui123123@gmail.com>
2023-09-27 16:18:37 +0700

    Bai3

---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai3.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace _07_2001210455_BuiQuocCong
+{
+    partial class Bai3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txbCode = new System.Windows.Forms.TextBox();
+            this.txbMH = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbGia = new System.Windows.Forms.Label();
+            this.txbSL = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnTTien = new System.Windows.Forms.Button();
+            this.lbTTien = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txbTDua = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lbTThua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHƯƠNG TRÌNH QUẢN LÝ BÁN HÀNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập vào mã code";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(300, 92);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Tìm kiếm";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // txbCode
+            // 
+            this.txbCode.Location = new System.Drawing.Point(175, 94);
+            this.txbCode.Name = "txbCode";
+            this.txbCode.Size = new System.Drawing.Size(100, 20);
+            this.txbCode.TabIndex = 3;
+            // 
+            // txbMH
+            // 
+            this.txbMH.Location = new System.Drawing.Point(175, 143);
+            this.txbMH.Name = "txbMH";
+            this.txbMH.Size = new System.Drawing.Size(144, 20);
+            this.txbMH.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tên mặt hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Giá";
+            // 
+            // lbGia
+            // 
+            this.lbGia.AutoSize = true;
+            this.lbGia.Location = new System.Drawing.Point(172, 196);
+            this.lbGia.Name = "lbGia";
+            this.lbGia.Size = new System.Drawing.Size(22, 13);
+            this.lbGia.TabIndex = 7;
+            this.lbGia.Text = "0.0";
+            // 
+            // txbSL
+            // 
+            this.txbSL.Location = new System.Drawing.Point(175, 239);
+            this.txbSL.Name = "txbSL";
+            this.txbSL.Size = new System.Drawing.Size(60, 20);
+            this.txbSL.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Số lượng";
+            // 
+            // btnTTien
+            // 
+            this.btnTTien.Location = new System.Drawing.Point(300, 237);
+            this.btnTTien.Name = "btnTTien";
+            this.btnTTien.Size = new System.Drawing.Size(75, 23);
+            this.btnTTien.TabIndex = 10;
+            this.btnTTien.Text = "Tính Tiền";
+            this.btnTTien.UseVisualStyleBackColor = true;
+            this.btnTTien.Click += new System.EventHandler(this.btnTTien_Click);
+            // 
+            // lbTTien
+            // 
+            this.lbTTien.AutoSize = true;
+            this.lbTTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTTien.Location = new System.Drawing.Point(172, 301);
+            this.lbTTien.Name = "lbTTien";
+            this.lbTTien.Size = new System.Drawing.Size(25, 13);
+            this.lbTTien.TabIndex = 12;
+            this.lbTTien.Text = "0.0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(71, 301);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Thành tiền";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(76, 359);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 22);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Tiền Thừa";
+            // 
+            // txbTDua
+            // 
+            this.txbTDua.Location = new System.Drawing.Point(175, 403);
+            this.txbTDua.Name = "txbTDua";
+            this.txbTDua.Size = new System.Drawing.Size(100, 20);
+            this.txbTDua.TabIndex = 16;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(300, 401);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(75, 23);
+            this.btnTinh.TabIndex = 15;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(71, 406);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(62, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Số tiền đưa";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(495, 403);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Tiền thừa:";
+            // 
+            // lbTThua
+            // 
+            this.lbTThua.AutoSize = true;
+            this.lbTThua.Location = new System.Drawing.Point(556, 403);
+            this.lbTThua.Name = "lbTThua";
+            this.lbTThua.Size = new System.Drawing.Size(22, 13);
+            this.lbTThua.TabIndex = 18;
+            this.lbTThua.Text = "0.0";
+            // 
+            // Bai3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 467);
+            this.Controls.Add(this.lbTThua);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txbTDua);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lbTTien);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnTTien);
+            this.Controls.Add(this.txbSL);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbGia);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbMH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbCode);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Bai3";
+            this.Text = "Bai3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txbCode;
+        private System.Windows.Forms.TextBox txbMH;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbGia;
+        private System.Windows.Forms.TextBox txbSL;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTTien;
+        private System.Windows.Forms.Label lbTTien;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txbTDua;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lbTThua;
+    }
+}
---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai3.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_2001210455_BuiQuocCong
+{
+    public partial class Bai3 : Form
+    {
+        public Bai3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            switch(txbCode.Text)
+            {
+                case "1":            
+                    txbMH.Text = "Bánh trung thu đậu xanh";
+                    lbGia.Text = "200000";
+                    break;
+                case "2":
+                    txbMH.Text = "Bánh trung thu thập cẩm";
+                    lbGia.Text = "250000";
+                    break;
+                case "3":
+                    txbMH.Text = "Bánh nướng";
+                    lbGia.Text = "150000";
+                    break;
+            }
+
+        }
+
+        private void btnTTien_Click(object sender, EventArgs e)
+        {
+            int n, m;
+            string gia = lbGia.Text;
+            string sl = txbSL.Text;
+
+            if (int.TryParse(sl, out n) && int.TryParse(gia, out m))
+            {
+                lbTTien.Text = (n * m).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số lượng hợp lệ và giá tiền là số nguyên.");
+            }
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            int n, m;
+            string thanhtien = lbTTien.Text;
+            string tiendua = txbTDua.Text;
+
+            if (int.TryParse(tiendua, out n) && int.TryParse(thanhtien, out m))
+            {
+                if (n < m)
+                {
+                    MessageBox.Show("Số tiền đưa phải lớn hơn hoặc bằng tổng tiền.");
+                }
+                else
+                {
+                    lbTThua.Text = (n - m).ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số tiền đưa và tổng tiền là số nguyên.");
+            }
+        }
+    }
+}
