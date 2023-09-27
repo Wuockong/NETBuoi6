1a7c8b71594d084b897417aae988fd3b57150f5a
Bui Quoc Cong <congbui123123@gmail.com>
2023-09-27 15:15:30 +0700

    Bai 1

---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace _07_2001210455_BuiQuocCong
+{
+    partial class Bai1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbCheck = new System.Windows.Forms.Label();
+            this.lbPrint1 = new System.Windows.Forms.Label();
+            this.lbPrint2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(530, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHƯƠNG TRÌNH KIỂM TRA VÀ IN SỐ NGUYÊN TỐ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập số nguyên tố:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(344, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kiểm Tra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "In SNT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(575, 273);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // lbCheck
+            // 
+            this.lbCheck.AutoSize = true;
+            this.lbCheck.Location = new System.Drawing.Point(106, 337);
+            this.lbCheck.Name = "lbCheck";
+            this.lbCheck.Size = new System.Drawing.Size(10, 13);
+            this.lbCheck.TabIndex = 7;
+            this.lbCheck.Text = " ";
+            // 
+            // lbPrint1
+            // 
+            this.lbPrint1.AutoSize = true;
+            this.lbPrint1.Location = new System.Drawing.Point(351, 337);
+            this.lbPrint1.Name = "lbPrint1";
+            this.lbPrint1.Size = new System.Drawing.Size(10, 13);
+            this.lbPrint1.TabIndex = 8;
+            this.lbPrint1.Text = " ";
+            // 
+            // lbPrint2
+            // 
+            this.lbPrint2.AutoSize = true;
+            this.lbPrint2.Location = new System.Drawing.Point(354, 380);
+            this.lbPrint2.Name = "lbPrint2";
+            this.lbPrint2.Size = new System.Drawing.Size(16, 13);
+            this.lbPrint2.TabIndex = 9;
+            this.lbPrint2.Text = "   ";
+            // 
+            // Bai1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 454);
+            this.Controls.Add(this.lbPrint2);
+            this.Controls.Add(this.lbPrint1);
+            this.Controls.Add(this.lbCheck);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Bai1";
+            this.Text = "Bai1";
+            this.Load += new System.EventHandler(this.Bai1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbCheck;
+        private System.Windows.Forms.Label lbPrint1;
+        private System.Windows.Forms.Label lbPrint2;
+    }
+}
---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_2001210455_BuiQuocCong
+{
+    public partial class Bai1 : Form
+    {
+        public Bai1()
+        {
+            InitializeComponent();
+        }
+
+        private bool kiemtraSNT(int a)
+        {
+            if(a <= 1)
+                return false;
+            if(a <= 3)
+                return true;
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+                if (a % i == 0)
+                    return false;
+            return true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nhap = int.Parse(textBox1.Text);
+            if(kiemtraSNT(nhap))
+                lbCheck.Text = string.Format("{0} là số nguyên tố", nhap);
+            else
+                lbCheck.Text = string.Format("{0} không phải là số nguyên tố", nhap);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int nhap = int.Parse(textBox1.Text);
+            lbPrint1.Text = string.Format("Các số nguyên tố từ 2..{0} là: ", nhap);
+            for (int i = 2; i <= nhap; i++)
+                if (kiemtraSNT(i))
+                    lbPrint2.Text = string.Format(i + " ");
+        }
+
+        private void Bai1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
