20b450b3567deb03013b730200c83da027a41144
Bui Quoc Cong <congbui123123@gmail.com>
2023-09-27 15:40:11 +0700

    Bai2

---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai2.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace _07_2001210455_BuiQuocCong
+{
+    partial class Bai2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.lbKQ = new System.Windows.Forms.Label();
+            this.txbC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbX1 = new System.Windows.Forms.Label();
+            this.lbX12 = new System.Windows.Forms.Label();
+            this.lbX2 = new System.Windows.Forms.Label();
+            this.lbVN = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GIẢI PHƯƠNG TRÌNH BẬC 2";
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(197, 126);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(100, 20);
+            this.txbA.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X^2 +";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "X +";
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(369, 126);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 20);
+            this.txbB.TabIndex = 3;
+            // 
+            // lbKQ
+            // 
+            this.lbKQ.AutoSize = true;
+            this.lbKQ.Location = new System.Drawing.Point(654, 126);
+            this.lbKQ.Name = "lbKQ";
+            this.lbKQ.Size = new System.Drawing.Size(22, 13);
+            this.lbKQ.TabIndex = 5;
+            this.lbKQ.Text = "= 0";
+            // 
+            // txbC
+            // 
+            this.txbC.Location = new System.Drawing.Point(539, 126);
+            this.txbC.Name = "txbC";
+            this.txbC.Size = new System.Drawing.Size(100, 20);
+            this.txbC.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Tính nghiệm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(384, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Làm mới";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(564, 209);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "PT có 2 nghiệm phân Biệt:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(455, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Nghiệm kép:";
+            // 
+            // lbX1
+            // 
+            this.lbX1.AutoSize = true;
+            this.lbX1.Location = new System.Drawing.Point(122, 320);
+            this.lbX1.Name = "lbX1";
+            this.lbX1.Size = new System.Drawing.Size(29, 13);
+            this.lbX1.TabIndex = 12;
+            this.lbX1.Text = "X1 =";
+            // 
+            // lbX12
+            // 
+            this.lbX12.AutoSize = true;
+            this.lbX12.Location = new System.Drawing.Point(455, 320);
+            this.lbX12.Name = "lbX12";
+            this.lbX12.Size = new System.Drawing.Size(54, 13);
+            this.lbX12.TabIndex = 13;
+            this.lbX12.Text = "X1 = X2 =";
+            // 
+            // lbX2
+            // 
+            this.lbX2.AutoSize = true;
+            this.lbX2.Location = new System.Drawing.Point(123, 342);
+            this.lbX2.Name = "lbX2";
+            this.lbX2.Size = new System.Drawing.Size(29, 13);
+            this.lbX2.TabIndex = 14;
+            this.lbX2.Text = "X2 =";
+            // 
+            // lbVN
+            // 
+            this.lbVN.AutoSize = true;
+            this.lbVN.Location = new System.Drawing.Point(131, 395);
+            this.lbVN.Name = "lbVN";
+            this.lbVN.Size = new System.Drawing.Size(10, 13);
+            this.lbVN.TabIndex = 15;
+            this.lbVN.Text = " ";
+            // 
+            // Bai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 449);
+            this.Controls.Add(this.lbVN);
+            this.Controls.Add(this.lbX2);
+            this.Controls.Add(this.lbX12);
+            this.Controls.Add(this.lbX1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txbC);
+            this.Controls.Add(this.lbKQ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.label1);
+            this.Name = "Bai2";
+            this.Text = "Bai2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.Label lbKQ;
+        private System.Windows.Forms.TextBox txbC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbX1;
+        private System.Windows.Forms.Label lbX12;
+        private System.Windows.Forms.Label lbX2;
+        private System.Windows.Forms.Label lbVN;
+    }
+}
---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_2001210455_BuiQuocCong
+{
+    public partial class Bai2 : Form
+    {
+        public Bai2()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txbA.Clear();
+            txbB.Clear();
+            txbC.Clear();
+            lbKQ.Text = "= 0";
+            lbX1.Text = "X1 =";
+            lbX2.Text = "X2 =";
+            lbX12.Text = "X1 = X2 =";
+            lbVN.Text = "";
+            txbA.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a,b,c;
+            if(double.TryParse(txbA.Text, out a) && double.TryParse(txbB.Text, out b) && double.TryParse(txbC.Text, out c))
+            {
+                double delta = b * b - 4 * a * c;
+                lbKQ.Text = string.Format("= {0}", delta);
+                if (delta > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    lbX1.Text = string.Format("X1 = {0}", x1);
+                    lbX2.Text = string.Format("X2 = {0}", x2);
+                }
+                else if (delta == 0)
+                {
+                    double x = -b / (2 * a);
+                    lbX12.Text = string.Format("X1 = X2 {0}", x);
+                }
+                else
+                {
+                    lbVN.Text = "Phương trình Vô Nghiệm";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập các hệ số hợp lệ (a, b, c) là số thực.");
+            }
+        }
+    }
+}
